b403bf9089158617252a2aa9572418367face2ab
drodspeed3@gmail.com <drodspeed3@gmail.com>
2020-06-21 00:40:09 -0400

    Add project files.

---
+++ app0303/app0303/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using github0303;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace app0303
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+            BindingContext = new MainViewModel2_0();
+        }
+
+        private async void page2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
